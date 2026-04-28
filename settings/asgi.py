74a53e83016eeb3be2516ddd3c2caa38b5c14764
Nurasyl555 <kalmahanbet.nur06@gmail.com>
2026-04-28 19:58:09 +0500

    fff

--- settings/asgi.py
+++ settings/asgi.py
@@ -1,22 +1,16 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-#import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.env.local')
+# Читаем из переменной окружения
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    os.environ.get('DJANGO_SETTINGS_MODULE', 'settings.env.local')
+)
 
 django_asgi_app = get_asgi_application()
+
 from apps.notifications.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
